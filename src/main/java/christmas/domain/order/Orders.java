b545356d87d8c82a618220f553651f89bad0a9a4
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 18:10:49 +0900

    feat: 중복된 메뉴가 있을 때 예외가 발생하는 기능 추가

--- src/main/java/christmas/domain/order/Orders.java
+++ src/main/java/christmas/domain/order/Orders.java
@@ -1,11 +1,24 @@
 package christmas.domain.order;
 
+import static christmas.ui.ErrorMessage.INVALID_ORDER;
+
 import java.util.List;
 
 public class Orders {
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
+        validateDuplicateMenu(orders);
         this.orders = orders;
+    }
+
+    private void validateDuplicateMenu(List<Order> orders) {
+        int distinctCount = (int) orders.stream()
+                .map(Order::getMenuName)
+                .distinct()
+                .count();
+        if (distinctCount != orders.size()) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
     }
 }
