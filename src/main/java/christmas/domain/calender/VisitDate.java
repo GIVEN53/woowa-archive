0d2f83b9376a902eee3f5df949a02ffa86ee6866
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 14:57:29 +0900

    feat: 방문 날짜 클래스 로드 시 캐싱

--- src/main/java/christmas/domain/calender/VisitDate.java
+++ src/main/java/christmas/domain/calender/VisitDate.java
@@ -1,13 +1,38 @@
 package christmas.domain.calender;
 
+import static christmas.domain.calender.EventDateConfig.END_DAY;
+import static christmas.domain.calender.EventDateConfig.MONTH;
+import static christmas.domain.calender.EventDateConfig.START_DAY;
+import static christmas.domain.calender.EventDateConfig.YEAR;
+
 import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class VisitDate {
+    private static final Map<Integer, VisitDate> DATE_CACHE = new HashMap<>();
+
+    static {
+        IntStream.rangeClosed(START_DAY.getValue(), END_DAY.getValue())
+                .mapToObj(day -> LocalDate.of(YEAR.getValue(), MONTH.getValue(), day))
+                .forEach(date -> {
+                    int day = date.getDayOfMonth();
+                    DayOfWeek dayOfWeek = date.getDayOfWeek();
+                    DATE_CACHE.put(day, new VisitDate(day, dayOfWeek));
+                });
+    }
+
     private final int date;
     private final DayOfWeek dayOfWeek;
 
-    public VisitDate(int date, DayOfWeek dayOfWeek) {
+    private VisitDate(int date, DayOfWeek dayOfWeek) {
         this.date = date;
         this.dayOfWeek = dayOfWeek;
+    }
+
+    public static VisitDate from(int date) {
+        return DATE_CACHE.get(date);
     }
 }
