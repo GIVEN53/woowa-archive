46f7a44fbe04107bde6cbd3329e9745d4981c0ac
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 23:11:16 +0900

    test: 메뉴 총 개수가 20개보다 많을 때 예외가 발생하는지 테스트

--- src/test/java/christmas/domain/order/OrdersTest.java
+++ src/test/java/christmas/domain/order/OrdersTest.java
@@ -32,4 +32,18 @@ class OrdersTest {
                 .isExactlyInstanceOf(IllegalArgumentException.class)
                 .hasMessage(INVALID_ORDER.getMessage());
     }
+
+    @Test
+    void 메뉴의_총_개수가_20개보다_많을_경우_예외가_발생한다() {
+        // given
+        Order order1 = Order.of("해산물파스타", 5);
+        Order order2 = Order.of("레드와인", 5);
+        Order order3 = Order.of("샴페인", 7);
+        Order order4 = Order.of("티본스테이크", 4);
+
+        // when & then
+        assertThatThrownBy(() -> new Orders(List.of(order1, order2, order3, order4)))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER.getMessage());
+    }
 }
