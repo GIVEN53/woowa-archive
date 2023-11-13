ec236de6923ece1525f838342f1483eccba3b35f
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 17:30:17 +0900

    feat: 주문한 메뉴 개수 1보다 작을 때 예외를 발생시키는 기능 추가

--- src/main/java/christmas/domain/order/OrderQuantity.java
+++ src/main/java/christmas/domain/order/OrderQuantity.java
@@ -1,9 +1,19 @@
 package christmas.domain.order;
 
+import static christmas.ui.ErrorMessage.INVALID_ORDER;
+
 class OrderQuantity {
+    private static final int MINIMUM_QUANTITY = 1;
     private final int quantity;
 
     public OrderQuantity(int quantity) {
+        validateQuantity(quantity);
         this.quantity = quantity;
+    }
+
+    private void validateQuantity(int quantity) {
+        if (quantity < MINIMUM_QUANTITY) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
     }
 }
