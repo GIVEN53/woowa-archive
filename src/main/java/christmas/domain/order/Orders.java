8082f4ce3ae3cf696579fb5fdcdf8bf9e77ef337
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 23:11:54 +0900

    feat: 메뉴의 총 개수가 20개보다 많을 때 예외를 발생시키는 기능 추가

--- src/main/java/christmas/domain/order/Orders.java
+++ src/main/java/christmas/domain/order/Orders.java
@@ -5,11 +5,13 @@ import static christmas.ui.ErrorMessage.INVALID_ORDER;
 import java.util.List;
 
 public class Orders {
+    private static final int MAX_MENU_COUNT = 20;
     private final List<Order> orders;
 
     public Orders(List<Order> orders) {
         validateDuplicateMenu(orders);
         validateOnlyBeverageMenu(orders);
+        validateMenuTotalCount(orders);
         this.orders = orders;
     }
 
@@ -28,6 +30,15 @@ public class Orders {
                 .allMatch(Order::hasBeverageMenu);
 
         if (onlyBeverageMenu) {
+            throw new IllegalArgumentException(INVALID_ORDER.getMessage());
+        }
+    }
+
+    private void validateMenuTotalCount(List<Order> orders) {
+        int totalCount = orders.stream()
+                .mapToInt(Order::getOrderQuantity)
+                .sum();
+        if (totalCount > MAX_MENU_COUNT) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
