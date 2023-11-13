77689a0a0cf5c04c91a2c9f1ce224a3a2ef1a96b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 17:03:25 +0900

    refactor: 테스트 중첩 구조로 변경

--- src/test/java/christmas/domain/menu/MenuGroupTest.java
+++ src/test/java/christmas/domain/menu/MenuGroupTest.java
@@ -2,35 +2,42 @@ package christmas.domain.menu;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
 
 class MenuGroupTest {
-    @ParameterizedTest
-    @EnumSource(value = Menu.class, names = {"MUSHROOM_SOUP", "TAPAS", "CAESAR_SALAD"})
-    void 에피타이저_메뉴이면_true를_반환한다(Menu menu) {
-        // when & then
-        assertThat(MenuGroup.isAppetizerMenu(menu)).isTrue();
+    @Nested
+    class 에피타이저_메뉴 {
+        @ParameterizedTest
+        @EnumSource(value = Menu.class, names = {"MUSHROOM_SOUP", "TAPAS", "CAESAR_SALAD"})
+        void true를_반환한다(Menu menu) {
+            // when & then
+            assertThat(MenuGroup.isAppetizerMenu(menu)).isTrue();
+        }
+
+        @ParameterizedTest
+        @EnumSource(value = Menu.class, names = {"T_BONE_STEAK", "CHOCO_CAKE", "ZERO_COKE"})
+        void false를_반환한다(Menu menu) {
+            // when & then
+            assertThat(MenuGroup.isAppetizerMenu(menu)).isFalse();
+        }
     }
 
-    @ParameterizedTest
-    @EnumSource(value = Menu.class, names = {"T_BONE_STEAK", "CHOCO_CAKE", "ZERO_COKE"})
-    void 에피타이저_메뉴가_아니면_false를_반환한다(Menu menu) {
-        // when & then
-        assertThat(MenuGroup.isAppetizerMenu(menu)).isFalse();
-    }
+    @Nested
+    class 메인_메뉴 {
+        @ParameterizedTest
+        @EnumSource(value = Menu.class, names = {"T_BONE_STEAK", "BARBECUED_RIB", "SEAFOOD_PASTA", "CHRISTMAS_PASTA"})
+        void true를_반환한다(Menu menu) {
+            // when & then
+            assertThat(MenuGroup.isMainMenu(menu)).isTrue();
+        }
 
-    @ParameterizedTest
-    @EnumSource(value = Menu.class, names = {"T_BONE_STEAK", "BARBECUED_RIB", "SEAFOOD_PASTA", "CHRISTMAS_PASTA"})
-    void 메인_메뉴이면_true를_반환한다(Menu menu) {
-        // when & then
-        assertThat(MenuGroup.isMainMenu(menu)).isTrue();
-    }
-
-    @ParameterizedTest
-    @EnumSource(value = Menu.class, names = {"TAPAS", "ICE_CREAM", "RED_WINE"})
-    void 메인_메뉴가_아니면_false를_반환한다(Menu menu) {
-        // when & then
-        assertThat(MenuGroup.isMainMenu(menu)).isFalse();
+        @ParameterizedTest
+        @EnumSource(value = Menu.class, names = {"TAPAS", "ICE_CREAM", "RED_WINE"})
+        void false를_반환한다(Menu menu) {
+            // when & then
+            assertThat(MenuGroup.isMainMenu(menu)).isFalse();
+        }
     }
 }
