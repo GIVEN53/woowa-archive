564b35e6b0ca3508e4fc980d8f5b9a2059625fed
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 17:49:49 +0900

    feat: 주문한 메뉴 개수가 50보다 클 때 예외 발생 추가

--- src/main/java/christmas/domain/order/OrderQuantity.java
+++ src/main/java/christmas/domain/order/OrderQuantity.java
@@ -4,13 +4,14 @@ import static christmas.ui.ErrorMessage.INVALID_ORDER;
 
 record OrderQuantity(int quantity) {
     private static final int MINIMUM_QUANTITY = 1;
+    private static final int MAXIMUM_QUANTITY = 50;
 
     OrderQuantity {
         validateQuantity(quantity);
     }
 
     private void validateQuantity(int quantity) {
-        if (quantity < MINIMUM_QUANTITY) {
+        if (quantity < MINIMUM_QUANTITY || quantity > MAXIMUM_QUANTITY) {
             throw new IllegalArgumentException(INVALID_ORDER.getMessage());
         }
     }
