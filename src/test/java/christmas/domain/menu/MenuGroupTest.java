05b9edb2d2fededb62a9531944a691f04918d142
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 16:53:52 +0900

    test: 에피타이저 메뉴인지 확인하는 테스트

---
+++ src/test/java/christmas/domain/menu/MenuGroupTest.java
@@ -0,0 +1,22 @@
+package christmas.domain.menu;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+class MenuGroupTest {
+    @ParameterizedTest
+    @EnumSource(value = Menu.class, names = {"MUSHROOM_SOUP", "TAPAS", "CAESAR_SALAD"})
+    void 에피타이저_메뉴이면_true를_반환한다(Menu menu) {
+        // when & then
+        assertThat(MenuGroup.isAppetizer(menu)).isTrue();
+    }
+
+    @ParameterizedTest
+    @EnumSource(value = Menu.class, names = {"T_BONE_STEAK", "CHOCO_CAKE", "ZERO_COKE"})
+    void 에피타이저_메뉴가_아니면_false를_반환한다(Menu menu) {
+        // when & then
+        assertThat(MenuGroup.isAppetizer(menu)).isFalse();
+    }
+}
