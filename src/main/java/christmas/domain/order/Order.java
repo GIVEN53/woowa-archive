68da9a6c90d9785f58f18d49e6ea3b6d1dbda5d2
NAM GIBEOM <rlqja0523@naver.com>
2023-11-13 17:42:46 +0900

    feat: 주문 클래스 추가

---
+++ src/main/java/christmas/domain/order/Order.java
@@ -0,0 +1,19 @@
+package christmas.domain.order;
+
+import christmas.domain.menu.Menu;
+
+public class Order {
+    private final Menu menu;
+    private final OrderQuantity orderQuantity;
+
+    private Order(Menu menu, OrderQuantity orderQuantity) {
+        this.menu = menu;
+        this.orderQuantity = orderQuantity;
+    }
+
+    public static Order of(String name, int quantity) {
+        Menu menu = Menu.findByName(name);
+        OrderQuantity orderQuantity = new OrderQuantity(quantity);
+        return new Order(menu, orderQuantity);
+    }
+}
