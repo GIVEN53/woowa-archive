28803b43a6c36e2ab267049016b51ac7f5224612
NAM GIBEOM <rlqja0523@naver.com>
2023-11-14 23:47:47 +0900

    test: 문자열을 콤마로 나누어 리스트로 변환하는지 테스트

--- src/test/java/christmas/util/ConverterTest.java
+++ src/test/java/christmas/util/ConverterTest.java
@@ -1,9 +1,12 @@
 package christmas.util;
 
 import static christmas.ui.ErrorMessage.INVALID_NUMBER;
+import static christmas.ui.ErrorMessage.INVALID_ORDER;
+import static christmas.util.Delimiter.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ConverterTest {
@@ -28,5 +31,29 @@ class ConverterTest {
 
         // then
         assertThat(number).isEqualTo(11);
+    }
+
+    @Test
+    void 문자열을_콤마로_나누어_리스트로_변환한다() {
+        // given
+        String input = "초코케이크-2" + COMMA.getValue() + "바비큐립-1";
+
+        // when
+        List<String> orders = Converter.convertToListByComma(input);
+
+        // then
+        assertThat(orders).containsExactly("초코케이크-2", "바비큐립-1");
+    }
+
+    @Test
+    void 문자열을_콤마로_나누어_리스트로_변환할_수_없을_경우_예외가_발생한다() {
+        // given
+        String invalidDelimiter = ".";
+        String input = "초코케이크-2" + invalidDelimiter + "바비큐립-1";
+
+        // when & then
+        assertThatThrownBy(() -> Converter.convertToListByComma(input))
+                .isExactlyInstanceOf(IllegalArgumentException.class)
+                .hasMessage(INVALID_ORDER.getMessage());
     }
 }
