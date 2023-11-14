61320213b91f2f51cbcb2e5c304f7a19e9600b6b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-14 13:24:46 +0900

    test: 방문 날짜가 26일 이상일 때 false를 반환하는지 테스트

---
+++ src/test/java/christmas/domain/discount/ChristmasDiscountTest.java
@@ -0,0 +1,27 @@
+package christmas.domain.discount;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import christmas.domain.calender.VisitDate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ChristmasDiscountTest {
+    private final ChristmasDiscount christmasDiscount = new ChristmasDiscount();
+
+    @ParameterizedTest
+    @ValueSource(ints = {26, 27, 28, 29, 30, 31})
+    void 방문날짜가_26일_이상일_경우_false를_반환한다(int day) {
+        // given
+        VisitDate visitDate = VisitDate.from(day);
+
+        // when
+        boolean result = christmasDiscount.isDiscountDate(visitDate);
+
+        // then
+        assertThat(result).isFalse();
+    }
+
+}
