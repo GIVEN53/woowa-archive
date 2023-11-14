e8a16df5c1f00684ff06e7800b938c8f511a4246
NAM GIBEOM <rlqja0523@naver.com>
2023-11-14 22:51:56 +0900

    feat: 혜택 금액에 따라 배지를 반환하는 기능 추가

--- src/main/java/christmas/domain/badge/Badge.java
+++ src/main/java/christmas/domain/badge/Badge.java
@@ -1,15 +1,39 @@
 package christmas.domain.badge;
 
+import java.util.Arrays;
+
 public enum Badge {
-    STAR("별", 5_000),
-    TREE("트리", 10_000),
-    SANTA("산타", 20_000);
+
+    SANTA("산타", 20_000, 1),
+    TREE("트리", 10_000, 2),
+    STAR("별", 5_000, 3),
+    NONE("없음", 0, 4);
 
     private final String name;
     private final int requiredBenefitAmount;
+    private final int priority;
+    private static final Badge[] BADGES = Badge.values();
 
-    Badge(String name, int requiredBenefitAmount) {
+    Badge(String name, int requiredBenefitAmount, int priority) {
         this.name = name;
         this.requiredBenefitAmount = requiredBenefitAmount;
+        this.priority = priority;
+    }
+
+    public static Badge findByBenefitAmount(int benefitAmount) {
+        return Arrays.stream(BADGES)
+                .sorted((o1, o2) -> o2.priority - o1.priority)
+                .filter(badge -> !badge.isNone(badge))
+                .filter(badge -> badge.isSatisfied(benefitAmount))
+                .findFirst()
+                .orElse(NONE);
+    }
+
+    private boolean isSatisfied(int benefitAmount) {
+        return this.requiredBenefitAmount <= benefitAmount;
+    }
+
+    private boolean isNone(Badge badge) {
+        return badge == NONE;
     }
 }
