21b4e852209ab50fb4cf52b0a7846eadd5a774ed
NAM GIBEOM <rlqja0523@naver.com>
2023-11-14 13:50:33 +0900

    feat: 방문 날짜에 따라 할인 금액을 계산하는 기능 추가

--- src/main/java/christmas/domain/discount/ChristmasDiscount.java
+++ src/main/java/christmas/domain/discount/ChristmasDiscount.java
@@ -4,8 +4,14 @@ import christmas.domain.calender.VisitDate;
 
 public class ChristmasDiscount {
     private static final int CHRISTMAS_DAY = 25;
+    private static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
+    private static final int DISCOUNT_AMOUNT_PER_DAY = 100;
 
     public boolean isDiscountDate(VisitDate visitDate) {
         return visitDate.getDate() <= CHRISTMAS_DAY;
+    }
+
+    public int calculateDiscountAmount(VisitDate visitDate) {
+        return DEFAULT_DISCOUNT_AMOUNT + DISCOUNT_AMOUNT_PER_DAY * (visitDate.getDate() - 1);
     }
 }
