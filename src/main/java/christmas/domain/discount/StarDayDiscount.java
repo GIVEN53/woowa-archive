10ef98c96935b833e2ff209439fd0c8c3da55b5b
NAM GIBEOM <rlqja0523@naver.com>
2023-11-14 22:26:16 +0900

    feat: 별이 있는 날에 할인 금액을 계산하는 기능 추가

---
+++ src/main/java/christmas/domain/discount/StarDayDiscount.java
@@ -0,0 +1,16 @@
+package christmas.domain.discount;
+
+import christmas.domain.calender.VisitDate;
+import christmas.domain.order.Orders;
+
+public final class StarDayDiscount implements Discount{
+    private static final int DEFAULT_DISCOUNT_AMOUNT = 1000;
+
+    @Override
+    public int calculateDiscountAmount(Orders orders, VisitDate visitDate) {
+        if (visitDate.isStarDay()) {
+            return DEFAULT_DISCOUNT_AMOUNT;
+        }
+        return NO_DISCOUNT;
+    }
+}
