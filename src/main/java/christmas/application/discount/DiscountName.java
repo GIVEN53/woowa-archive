942b7699a22659f634a8042cba10bf9d33880144
NAM GIBEOM <rlqja0523@naver.com>
2023-11-15 20:12:39 +0900

    feat: enum 값 추가

--- src/main/java/christmas/application/discount/DiscountName.java
+++ src/main/java/christmas/application/discount/DiscountName.java
@@ -4,7 +4,8 @@ public enum DiscountName {
     CHRISTMAS_EVENT("크리스마스 디데이 할인"),
     WEEKDAY_EVENT("평일 할인"),
     WEEKEND_EVENT("주말 할인"),
-    STAR_DAY_EVENT("특별 할인");
+    STAR_DAY_EVENT("특별 할인"),
+    GIVEAWAY_EVENT("증정 이벤트");
 
     private final String name;
 
