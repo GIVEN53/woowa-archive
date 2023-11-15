ff7c2945e1a9f0041b7b8f117f6cf7974a5b6fd2
NAM GIBEOM <rlqja0523@naver.com>
2023-11-15 20:17:20 +0900

    feat: 가격 필드 추가

--- src/main/java/christmas/dto/Giveaway.java
+++ src/main/java/christmas/dto/Giveaway.java
@@ -2,13 +2,15 @@ package christmas.dto;
 
 import christmas.domain.menu.Menu;
 
-public record Giveaway(String name) {
+public record Giveaway(String name, int price) {
     private static final String NO_GIVEAWAY = "없음";
+    private static final int NO_PRICE = 0;
+
     public static Giveaway from(Menu menu) {
-        return new Giveaway(menu.getName());
+        return new Giveaway(menu.getName(), menu.getPrice());
     }
 
     public static Giveaway noGiveaway() {
-        return new Giveaway(NO_GIVEAWAY);
+        return new Giveaway(NO_GIVEAWAY, NO_PRICE);
     }
 }
