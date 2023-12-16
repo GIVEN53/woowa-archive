0d39d98434f3c61be91dff4573ed4dc2d2031e00
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 14:26:41 +0900

    feat: 휴일 비상 근무 순번을 위해 사원 닉네임을 입력받는 기능 추가

--- src/main/java/oncall/ui/InputView.java
+++ src/main/java/oncall/ui/InputView.java
@@ -12,13 +12,20 @@ public class InputView {
 
     public String scanMonthAndDayOfWeek() {
         System.out.print("비상 근무를 배정할 월과 시작 요일을 입력하세요> ");
-        String input = Console.readLine();
-        inputValidator.validate(input);
-        return input;
+        return scanInput();
     }
 
     public String scanEmployeeNickNameForWeekdayShift() {
         System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        return scanInput();
+    }
+
+    public String scanEmployeeNickNameForWeekendShift() {
+        System.out.print("휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        return scanInput();
+    }
+
+    private String scanInput() {
         String input = Console.readLine();
         inputValidator.validate(input);
         return input;
