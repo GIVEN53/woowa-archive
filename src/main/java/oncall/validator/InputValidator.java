48f1cb6f42a0624fcba293c2aef51022ca4be87b
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 14:19:17 +0900

    feat: 입력값에 공백이 포함되어 있는지 확인하는 기능 추가

--- src/main/java/oncall/validator/InputValidator.java
+++ src/main/java/oncall/validator/InputValidator.java
@@ -1,15 +1,25 @@
 package oncall.validator;
 
+import static oncall.ui.ErrorMessage.CONTAIN_SPACE;
 import static oncall.ui.ErrorMessage.IS_BLANK;
 
 public class InputValidator {
+    private static final String SPACE = " ";
+
     public void validate(final String input) {
         validateBlank(input);
+        validateContainSpace(input);
     }
 
     private void validateBlank(final String input) {
         if (input.isBlank()) {
             throw new IllegalArgumentException(IS_BLANK.getMessage());
+        }
+    }
+
+    private void validateContainSpace(final String input) {
+        if (input.contains(SPACE)) {
+            throw new IllegalArgumentException(CONTAIN_SPACE.getMessage());
         }
     }
 }
