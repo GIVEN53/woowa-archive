ff55fe2b21c0539bd44fe90d5c4f70215fc6bb74
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 15:32:26 +0900

    feat: int 변환하는 기능 추가

---
+++ src/main/java/oncall/util/Converter.java
@@ -0,0 +1,10 @@
+package oncall.util;
+
+public class Converter {
+    private Converter() {
+    }
+
+    public static int convertToInt(String value) {
+        return Integer.parseInt(value);
+    }
+}
