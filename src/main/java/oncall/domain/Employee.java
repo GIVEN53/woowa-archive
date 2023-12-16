372725cd2b837b1a74616515c3df10296ae88a70
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 15:09:07 +0900

    feat: 사원 닉네임 사이즈를 확인하는 기능 추가

--- src/main/java/oncall/domain/Employee.java
+++ src/main/java/oncall/domain/Employee.java
@@ -1,11 +1,22 @@
 package oncall.domain;
 
+import static oncall.ui.ErrorMessage.*;
+
+import oncall.ui.ErrorMessage;
+
 public class Employee {
     private String nickname;
     private boolean weekdayShift;
     private boolean weekendShift;
 
     public Employee(String nickname) {
+        validateNicknameSize(nickname);
         this.nickname = nickname;
+    }
+
+    private void validateNicknameSize(String nickname) {
+        if (nickname.length() > 5) {
+            throw new IllegalArgumentException(INVALID_NICKNAME_SIZE.getMessage());
+        }
     }
 }
