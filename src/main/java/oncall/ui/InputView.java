6c7155c1cd3783199e7e48144d1c1e1f7db8977d
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 14:25:31 +0900

    feat: 평일 비상 근무 순번을 위해 사원 닉네임을 입력받는 기능 추가

--- src/main/java/oncall/ui/InputView.java
+++ src/main/java/oncall/ui/InputView.java
@@ -16,4 +16,11 @@ public class InputView {
         inputValidator.validate(input);
         return input;
     }
+
+    public String scanEmployeeNickNameForWeekdayShift() {
+        System.out.print("평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ");
+        String input = Console.readLine();
+        inputValidator.validate(input);
+        return input;
+    }
 }
