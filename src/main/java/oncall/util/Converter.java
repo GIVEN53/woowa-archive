adae492907584e6b63c39e433b88c7f36ce06b06
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 15:38:18 +0900

    feat: 구분자로 리스트 변환하는 기능 추가

--- src/main/java/oncall/util/Converter.java
+++ src/main/java/oncall/util/Converter.java
@@ -1,10 +1,22 @@
 package oncall.util;
 
+import static oncall.ui.ErrorMessage.SEPARATE_BY_COMMA;
+import static oncall.util.Delimiter.COMMA;
+
+import java.util.List;
+
 public class Converter {
     private Converter() {
     }
 
     public static int convertToInt(String value) {
         return Integer.parseInt(value);
+    }
+
+    public static List<String> convertToListByDelimiter(String value) {
+        if (value.matches(COMMA.getRegex())) {
+            return List.of(value.split(COMMA.getValue()));
+        }
+        throw new IllegalArgumentException(SEPARATE_BY_COMMA.getMessage());
     }
 }
--- src/main/java/oncall/util/Delimiter.java
+++ src/main/java/oncall/util/Delimiter.java
@@ -1,15 +1,21 @@
 package oncall.util;
 
 public enum Delimiter {
-    COMMA(",");
+    COMMA(",", "^[가-힣a-zA-Z\\d]+(,[가-힣a-zA-Z\\d]+)*$");
 
     private final String value;
+    private final String regex;
 
-    Delimiter(String value) {
+    Delimiter(String value, String regex) {
         this.value = value;
+        this.regex = regex;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public String getRegex() {
+        return regex;
     }
 }
