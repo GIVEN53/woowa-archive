dc818d5cd187706ff076a0c8690b1a5aa03212e8
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 15:05:21 +0900

    feat: 기준 요일을 첫 번째 순서로 요일 리스트를 반환하는 기능 추가

--- src/main/java/oncall/domain/DayOfWeek.java
+++ src/main/java/oncall/domain/DayOfWeek.java
@@ -2,7 +2,9 @@ package oncall.domain;
 
 import static oncall.ui.ErrorMessage.INVALID_DAY_OF_WEEK;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public enum DayOfWeek {
@@ -16,6 +18,7 @@ public enum DayOfWeek {
 
     private static final List<DayOfWeek> WEEKDAY = List.of(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY);
     private static final List<DayOfWeek> WEEKEND = List.of(SATURDAY, SUNDAY);
+    private static final DayOfWeek[] DAY_OF_WEEKS = values();
     private final String name;
 
     DayOfWeek(String name) {
@@ -27,6 +30,14 @@ public enum DayOfWeek {
                 .filter(dayOfWeek -> dayOfWeek.name.equals(name))
                 .findFirst()
                 .orElseThrow(() -> new IllegalArgumentException(INVALID_DAY_OF_WEEK.getMessage()));
+    }
+
+    public static List<DayOfWeek> getDayOfWeeksInOrderFrom(DayOfWeek start) {
+        List<DayOfWeek> dayOfWeeks = new ArrayList<>();
+        dayOfWeeks.addAll(Arrays.asList(DAY_OF_WEEKS).subList(start.ordinal(), DAY_OF_WEEKS.length));
+        dayOfWeeks.addAll(Arrays.asList(DAY_OF_WEEKS).subList(0, start.ordinal()));
+
+        return dayOfWeeks;
     }
 
     public String getName() {
