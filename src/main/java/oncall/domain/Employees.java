acf5c5307c0b07eda1209a49f7206896f5b008f8
NAM GIBEOM <rlqja0523@naver.com>
2023-12-16 15:12:23 +0900

    feat: 사원이 5명 이상인지 확인하는 기능 추가

--- src/main/java/oncall/domain/Employees.java
+++ src/main/java/oncall/domain/Employees.java
@@ -1,11 +1,19 @@
 package oncall.domain;
 
 import java.util.List;
+import oncall.ui.ErrorMessage;
 
 public class Employees {
-   private final List<Employee> employees;
+    private final List<Employee> employees;
 
     public Employees(List<Employee> employees) {
+        validateEmployeeMinSize(employees);
         this.employees = employees;
+    }
+
+    private void validateEmployeeMinSize(List<Employee> employees) {
+        if (employees.size() < 5) {
+            throw new IllegalArgumentException(ErrorMessage.LESS_THAN_MINIMUM_EMPLOYEE.getMessage());
+        }
     }
 }
