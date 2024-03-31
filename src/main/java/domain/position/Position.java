8dd97b9870edce39f926891eb6aac8c60d44ffe7
Nam Gi Beom <rlqja0523@naver.com>
2024-03-31 23:56:58 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/domain/position/Position.java
+++ src/main/java/domain/position/Position.java
@@ -1,6 +1,10 @@
 package domain.position;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -106,7 +110,7 @@ public class Position {
     }
 
     private int calculateFileGap(Position target) {
-        return Math.abs(this.file.subtract(target.file));
+        return Math.abs(this.file.order() - target.file.order());
     }
 
     public boolean isUpperRankThan(Position target) {
