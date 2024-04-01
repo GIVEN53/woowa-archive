c9e52d09bc6bc03481ec2c423f51bd543992475c
Nam Gi Beom <rlqja0523@naver.com>
2024-04-01 17:42:30 +0900

    refactor: 사용하지 않는 메서드 삭제

--- src/main/java/domain/board/Winner.java
+++ src/main/java/domain/board/Winner.java
@@ -4,16 +4,4 @@ public enum Winner {
     WHITE,
     BLACK,
     DRAW;
-
-    public boolean isWhite() {
-        return this == WHITE;
-    }
-
-    public boolean isBlack() {
-        return this == BLACK;
-    }
-
-    public boolean isDraw() {
-        return this == DRAW;
-    }
 }
